924d7400126b0c836f5db5472138ef8f4dc5cf4a
AustinJVC <avand955@mtroyal.ca>
2024-04-24 20:17:46 -0600

    added connection to frontend

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  cors: {origin: "*"}
+});
 
-// Define an object to store rooms
 const rooms = {};
 
 io.on('connection', (socket) => {
   console.log('User connected');
+  socket.emit('successfulConnect')
 
-  // Handle user creating a room
+  //Create room
   socket.on('createRoom', () => {
-    let roomCode = 'DADDY'; //Add generate random code
-
+    console.log("Create room request received. Generating room code.")
+    const roomCode = generateRoomCode(); //Make an API request for a random 6-letter word.
+    console.log("Generating room. CODE: " + roomCode)
     if (!rooms[roomCode]) {
-      // Create a new room object with initial data
+      // Create a new room object.
       rooms[roomCode] = { roomCode, gameState: {}, roomPlayers: {} };
-      socket.join(roomCode); // User joins the created room
       console.log(`Room ${roomCode} created`);
       socket.emit('roomCreated', roomCode); // Send roomCode to user
     } else {
       // Handle case of generating a duplicate room code (optional)
       console.log(`Room code collision detected, generating new code`);
-      socket.emit('roomCreateFailed', 'Duplicate room code'); // Inform user
+      //socket.emit('roomCreateFailed', 'Duplicate room code'); // Inform user
       // You can call createRoom again to retry or implement backoff strategy
     }
-  });
-
-  // ... other logic for users joining existing rooms, 
-  // handling game logic, and disconnecting from rooms
+  });  
 });
+
+
+async function generateRoomCode() {
+  const response = await fetch("https://random-word-api.herokuapp.com/word?length=6");
+  return response.json()[0].toUpperCase();
+}
 
 // Start the server
 http.listen(3000, () => {
