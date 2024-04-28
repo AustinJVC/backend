3d03004d0bf1f453e86acba5cc59d5fc313728bb
AustinJVC <avand955@mtroyal.ca>
2024-04-28 16:08:38 -0600

    MASSSSIVEEEE

--- index.js
+++ index.js
@@ -5,37 +5,60 @@ const io = require('socket.io')(http, {
   cors: {origin: "*"}
 });
 
-const rooms = {};
+let rooms = [];
+let users = [];
 
 io.on('connection', (socket) => {
   console.log('User connected');
-  socket.emit('successfulConnect')
 
-  //Create room
-  socket.on('createRoom', () => {
-    console.log("Create room request received. Generating room code.")
-    const roomCode = generateRoomCode(); //Make an API request for a random 6-letter word.
-    console.log("Generating room. CODE: " + roomCode)
-    if (!rooms[roomCode]) {
-      // Create a new room object.
-      rooms[roomCode] = { roomCode, gameState: {}, roomPlayers: {} };
-      console.log(`Room ${roomCode} created`);
-      socket.emit('roomCreated', roomCode); // Send roomCode to user
-    } else {
-      // Handle case of generating a duplicate room code (optional)
-      console.log(`Room code collision detected, generating new code`);
-      //socket.emit('roomCreateFailed', 'Duplicate room code'); // Inform user
-      // You can call createRoom again to retry or implement backoff strategy
+  socket.on('join-room', (username, roomCode) => {
+    const user ={
+      username, 
+      id: socket.id,
     }
-  });  
-});
+    
+    users.push(user);
+    socket.join(roomCode)
 
+    io.to(roomCode).emit('user-joined', user);
+
+    rooms[roomCode].users += 1;
+    rooms[roomCode].userList.push(user.username);
+
+    printConsoleLogs(username, socket.id, roomCode);
+  });
+
+  socket.on('create-room', async (username) => {
+    const user ={
+      username, 
+      id: socket.id,
+    }
+    users.push(user);
+    io.to(roomCode).emit('user-joined', user);
+    const roomCode = await generateRoomCode();
+    rooms[roomCode] = { users: 1, createdAt: Date.now(), userList: [] };
+    socket.join(roomCode);
+    rooms[roomCode].userList.push(user.username);
+
+    printConsoleLogs(username, socket.id, roomCode);
+  });
+}); 
+
+function printConsoleLogs(username, socketID, roomCode){
+  console.log("User: " + username);
+  console.log("SocketID: " + socketID);
+  console.log("Roomcode: " + roomCode);
+
+  console.log(users);
+  console.log(rooms);
+
+}
 
 async function generateRoomCode() {
   const response = await fetch("https://random-word-api.herokuapp.com/word?length=6");
-  return response.json()[0].toUpperCase();
+  const data = await response.json();  // Await and store the parsed JSON data
+  return data[0].toString();
 }
-
 // Start the server
 http.listen(3000, () => {
   console.log('Server listening on port 3000');
