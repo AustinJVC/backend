77d460ed9425d937a2a0301dd0b37f59c0b1d63b
AustinJVC <avand955@mtroyal.ca>
2024-04-30 09:08:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const io = require('socket.io')(http, {
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://whispers.austinvc.ca'); // Replace with your frontend origin
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow OPTIONS method
   next();
 });
 
